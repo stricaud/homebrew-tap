01ff3da77f780f8ec73fdf868a7c0bbc3e9b6477
Seb Tricaud <sebastien.tricaud@gmail.com>
2026-07-13 11:48:47 +0200

    Update version

--- Formula/libpcapng.rb
+++ Formula/libpcapng.rb
@@ -1,8 +1,8 @@
 class Libpcapng < Formula
   desc "pcapng read/write, reassembly, dissection and posa decoders"
   homepage "https://github.com/stricaud/libpcapng"
-  url "https://github.com/stricaud/libpcapng/archive/refs/tags/v0.6.tar.gz"
-  sha256 "99dd09f0871806d4a150f5cf9ed207cdfc3e61ccf653e97e5c3a37f06aee9367"
+  url "https://github.com/stricaud/libpcapng/archive/refs/tags/v0.7.tar.gz"
+  sha256 "ba5f721bda797f2260ec88d743108086ed3bb82da051a559a8f5eb6b50523555"
   license "MIT"
   head "https://github.com/stricaud/libpcapng.git", branch: "main"
 
